86627d86d14894ca94f3d536b1ee13eada292d1c
Ed <107416974+EdCryptic03@users.noreply.github.com>
2025-12-24 15:15:00 +0100

    Refactor server and client code structure

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-Server.doc
+
 
 
 
@@ -67,50 +67,6 @@ t1.start()
 t2.start()
 
 
-_____________________________________________________________________________________________________
-
-
-Client.doc
-
-
-import socket 
-import threading 
-import random
-import time
-from datetime import datetime 
-random_socket = random.randint(8000, 9000)
-ip = socket.gethostbyname(socket.gethostname())
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.bind((ip, random_socket))
-
-name = input("Nickname: ")
-
-def receive():
-    while True:
-        try:
-            message, _ = client.recvfrom(1024)
-            decoded_msg = message.decode()
-            if not decoded_msg.startswith("["):
-                timestamp = datetime.now().strftime("%H:%M:%S")
-                print(f"[{timestamp}] {decoded_msg}")
-            else:
-                print(decoded_msg)
-        except:
-            pass
-
-t = threading.Thread(target=receive)
-t.start()
-
-client.sendto(f"SIGNUP_TAG: {name}".encode(), (ip, 6666))
-
-while True:
-    message = input("")
-    if message == '!q':
-        exit()
-    else:
-        # Send typing indicator before the message
-        client.sendto(f"TYPING: {name}".encode(), (ip, 6666))
-        # Small delay to show typing indicator
-        time.sleep(0.1)
         # Send the actual message
         client.sendto(f"{name}: {message}".encode(), (ip, 6666))
+
